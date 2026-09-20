3175b6b6bcc5b602544d98db0916b4a289da2169
Jose Morales <joossee142@gmail.com>
2026-09-20 13:48:04 -0500

    Update GenerateInfoFiles.java

--- PrimeraEntrega/src/primeraentrega/GenerateInfoFiles.java
+++ PrimeraEntrega/src/primeraentrega/GenerateInfoFiles.java
@@ -204,8 +204,6 @@ public class GenerateInfoFiles{
             String[] data = line.split(";");
             long id = Long.parseLong(data[1]);
             String name = data[2];
-            String lastname = data[3];
-            String fullName = name + "_" + lastname;
             
             int randomSalesCount = RANDOM.nextInt(100) + 1;
             
